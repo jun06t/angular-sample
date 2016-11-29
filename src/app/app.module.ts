23b52274142d424f1a344d386ad118a77de58690
Junpei Tsuji <tsuji_jumpei@cyberagent.co.jp>
2016-11-30 01:26:36 +0900

    adopt material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import 'hammerjs' 
+
+// Material 2 
+import { MdCoreModule, MdUniqueSelectionDispatcher, OVERLAY_PROVIDERS } from '@angular2-material/core'
+import { MdButtonModule } from '@angular2-material/button';
+import { MdCardModule } from '@angular2-material/card';
+import { MdRadioModule } from '@angular2-material/radio';
+import { MdCheckboxModule } from '@angular2-material/checkbox'
+import { MdTooltipModule } from '@angular2-material/tooltip';
+import { MdSliderModule } from '@angular2-material/slider';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,9 +23,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdCoreModule, MdCardModule, MdButtonModule, MdRadioModule,
+    MdCheckboxModule, MdTooltipModule, MdSliderModule
   ],
-  providers: [],
+  providers: [MdUniqueSelectionDispatcher, OVERLAY_PROVIDERS],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
