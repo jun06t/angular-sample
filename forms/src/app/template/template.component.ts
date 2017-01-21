823e0a65eb5b1b6127a1ae24555a402c36f8c9ee
Junpei Tsuji <tsuji_jumpei@cyberagent.co.jp>
2017-01-21 13:50:09 +0900

    Added template driven form

---
+++ forms/src/app/template/template.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  loginForm(value: any) {
+    console.log(value);
+  }
+}
