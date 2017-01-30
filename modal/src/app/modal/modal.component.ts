6e61ea3a04cb378433c47a7b3d8c21f2f23e23cc
Junpei Tsuji <tsuji_jumpei@cyberagent.co.jp>
2017-01-30 19:08:42 +0900

    tmp

--- modal/src/app/modal/modal.component.ts
+++ modal/src/app/modal/modal.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
+
+import { Content1Component } from '../content1/content1.component';
+import { Content2Component } from '../content2/content2.component';
 
 @Component({
   selector: 'app-modal',
@@ -6,8 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
+  currentComponent = null;
 
-  constructor() { }
+  @ViewChild('content', { read: ViewContainerRef }) content: ViewContainerRef;
+
+  constructor(private resolver: ComponentFactoryResolver) { }
+
+  @Input() set componentData(data: any) {
+    if (this.currentComponent) {
+      this.currentComponent.destroy();
+    }
+
+    let factory = this.resolver.resolveComponentFactory(data);
+
+  }
 
   ngOnInit() {
   }
