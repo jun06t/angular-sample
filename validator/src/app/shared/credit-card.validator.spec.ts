9ace92cac3e5449c52771cca0fa5cc1e8252ab4f
Junpei Tsuji <tsuji_jumpei@cyberagent.co.jp>
2017-02-03 22:48:04 +0900

    Removed unused variable

--- validator/src/app/shared/credit-card.validator.spec.ts
+++ validator/src/app/shared/credit-card.validator.spec.ts
@@ -1,7 +1,6 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 
 import { CreditCardValidator } from './credit-card.validator';
