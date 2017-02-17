407877d439baef436530b48745d339a56dcc7cf7
Junpei Tsuji <tsuji_jumpei@cyberagent.co.jp>
2017-02-17 19:00:13 +0900

    Added each component for modal

--- modal-basic/src/app/app.module.ts
+++ modal-basic/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ModalComponent } from './modal/modal.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { CompleteComponent } from './complete/complete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalComponent,
+    ConfirmationComponent,
+    CompleteComponent
   ],
   imports: [
     BrowserModule,
